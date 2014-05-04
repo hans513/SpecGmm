1fb65e104b47d3665334daa7160039cd653dd2bc
Hans Huang <hans2@cmu.edu>
2014-05-04 13:58:59 -0400

    Initial Commit

---
+++ SpecGmm/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SpecGmm
+//
+//  Created by Huang, Tse-Han on 2014/5/4.
+//  Copyright (c) 2014年 Huang, Tse-Han. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
